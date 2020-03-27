46e70184efa386d58eefe99500f6d1d7c0a56846
brunobelevan <brunobelevan@gmail.com>
2020-03-26 20:35:30 -0700

    I learnt until this point how to import functions

---
+++ Alessio.py
@@ -0,0 +1,43 @@
+from myFunctions import expo
+from myFunctions import sumar2nums 
+
+#print("Hola como estas")
+#print(50999)
+a = 90
+b = "bruno" 
+apellido = "Belevan"
+print(a)
+print(b.capitalize())
+print(b.upper())
+print(b.lower())
+print(b.replace('o','e'))
+oracion = "Bruno es mi amigo"
+print(oracion.split())
+nombre2 = b.capitalize()
+print(nombre2)
+gravedad = 9.8 
+boolean = True
+print(type(gravedad))
+print((type(b)))
+print(type(boolean))
+print(1+1)
+print(2+2)
+print(1/2)
+print(2**2)
+
+print(nombre2 + " " + apellido)
+
+print("La gravedad es" + " " + str(gravedad))
+
+print(nombre2, apellido)
+
+print("la gravedad es %.2f y %i" % (gravedad, a))
+
+exponencial = expo(2,3)
+print(exponencial)
+
+sumar = sumar2nums(4, 8)
+print(sumar) 
+
+
+
---
+++ myFunctions.py
@@ -0,0 +1,11 @@
+##############################################
+# These are my functions 
+##############################################
+
+def expo(num1, num2):
+    res = num1**num2
+    return res
+
+def sumar2nums(num1, num2): 
+    res = num1 + num2 
+    return res 
